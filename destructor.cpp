d328c39a1ab7a34a1b60b0e9339728f4aaf593df
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-05 18:51:26 +0700

    class public

--- destructor.cpp
+++ destructor.cpp
@@ -6,3 +6,10 @@ class angka
     private:
         int *arr;
         int panjang;
+
+        public:
+            angka(int); // Constructor
+            ~angka(); // Destructor
+            void cetakdata();
+            void isidata();
+};
