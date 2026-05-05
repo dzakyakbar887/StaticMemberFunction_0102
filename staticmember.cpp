91585cf783a2f1470c6bf66cbe050299b96f3a93
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-05 19:55:52 +0700

    staticmember

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa { // membuat class Mahasiswa
+class Mahasiswa { //membuat class mahasiswa
 
-    public:
-        static int nim; // deklarasi variable static yaitu variable nim
-        // deklarasi variable member
-        int id;
-        string nama;
+public:
+    static int nim; // deklarasi variabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
+    
+    //deklarasi prosedur setID() dan printALL()
+    void setID();
+    void printALL();
 
-        // deklarasi prosedur setID() dan printAll()
-        void setID();
-        void printAll();
-
-        //pembuatan constructor mahasiswa dengan parameter nama
-        //member intitializer list
-        Mahasiswa(string nama) : nama(nama) {
-            setID();
-        }
+    //pembuatan cosntructor Mahasiswa dengan parameter pnama
+    //member initialize list
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
 };
+
+//memberi value ke variabel nim dari class mahasiswa
+int Mahasiswa::nim = 0;
+
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+};
+
+void Mahasiswa::printALL() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+};
+
+int main() {
+    //membuat object dan memberi nilai
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    //memanggil prosedur printALL()
+    mhs1.printALL();
+    mhs2.printALL();
+    mhs3.printALL();
+    mhs4.printALL();
+
+    return 0;
+}
