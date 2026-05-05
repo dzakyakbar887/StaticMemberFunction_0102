120162c8d0224fc13504c425c69bb52774218db0
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-05 19:57:58 +0700

    last

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -2,27 +2,63 @@
 #include <string> //menyertakan library string
 using namespace std;
 
-class Mahasiswa { 
+class Mahasiswa {
 
-    private:
-        static int nim; 
-        public:
-        int id;
-        string nama;
+private: 
+    static int nim;
+public:
+    int id;
+    string nama;
 
-        void setID();
-        void printAll();
+    void setID();
+    void printALL();
 
-        //pembuatan static function
-        static void setnim(int nim) {
-            nim = nim;
-        }
+    //pembuatan static function
+    static void setNIM(int pnim) {
+        nim = pnim;
+    };
 
-        static int getnim() {
-            return nim;
-        }
 
-        Mahasiswa(string nama) : nama(nama) {
-            setID();
-        }
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
 };
+
+int Mahasiswa::nim = 0;
+
+//pendefinisial prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printALL() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    //pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri dadi");
+    Mahasiswa mhs2("Budi jatmiko");
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNIM(9);
+    Mahasiswa mhs3("Andi janu");
+    Mahasiswa mhs4("Joko wahono");
+
+    //memanggil prosedur printALL()
+    mhs1.printALL();
+    mhs2.printALL();
+    mhs3.printALL();
+    mhs4.printALL();
+
+    //menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+}
